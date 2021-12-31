84176e9148cdc94e5bf90a16c52e7c725523fbb4
amrhoballah <amrhoballah@yahoo.com>
2021-12-31 20:39:17 +0200

    Update init.js

--- src/functions/init.js
+++ src/functions/init.js
@@ -1,5 +1,9 @@
 function init() {
     console.log("init")
+    let regFile = [];
+    for(let i in 31){
+        regFile.add({Q: "", value: ""})
+    }
     return [{instruction: [], issue: [], execute: [], writeResult: []}, //instruction Queue
             {A1: {op:"", Vj:"", Vk:"", Qj:"", Qk:"", busy: 0, cyclesRemaining: ""}, 
                 A2: {op:"", Vj:"", Vk:"", Qj:"", Qk:"", busy: 0, cyclesRemaining: ""},
@@ -11,38 +15,7 @@ function init() {
                 L2: {address: "", busy: 0, cyclesRemaining: ""}}, //load reservation
             {S1: {address: "", V: "", Q: "", busy: 0, cyclesRemaining: ""}, 
                 S2: {address: "", V: "", Q: "", busy: 0, cyclesRemaining: ""}}, // store reservation
-            {F0: {Q: "", value: ""},
-                F1: {Q: "", value: ""},
-                F2: {Q: "", value: ""},
-                F3: {Q: "", value: ""},
-                F4: {Q: "", value: ""},
-                F5: {Q: "", value: ""},
-                F6: {Q: "", value: ""},
-                F7: {Q: "", value: ""},
-                F8: {Q: "", value: ""},
-                F9: {Q: "", value: ""},
-                F10: {Q: "", value: ""},
-                F11: {Q: "", value: ""},
-                F12: {Q: "", value: ""},
-                F13: {Q: "", value: ""},
-                F14: {Q: "", value: ""},
-                F15: {Q: "", value: ""},
-                F16: {Q: "", value: ""},
-                F17: {Q: "", value: ""},
-                F18: {Q: "", value: ""},
-                F19: {Q: "", value: ""},
-                F20: {Q: "", value: ""},
-                F21: {Q: "", value: ""},
-                F22: {Q: "", value: ""},
-                F23: {Q: "", value: ""},
-                F24: {Q: "", value: ""},
-                F25: {Q: "", value: ""},
-                F26: {Q: "", value: ""},
-                F27: {Q: "", value: ""},
-                F28: {Q: "", value: ""},
-                F29: {Q: "", value: ""},
-                F30: {Q: "", value: ""},
-                F31: {Q: "", value: ""},}, //register file
+            regFile, //register file
             {data: []} //memory
     ]
 }
