e55d7b9760d37a56b40659e9c402a7c5f42adfc4
amrhoballah <amrhoballah@yahoo.com>
2021-12-31 19:19:15 +0200

    Update startExecuting.js

--- src/functions/startExecuting.js
+++ src/functions/startExecuting.js
@@ -1,18 +1,21 @@
 function startExecuting(instruction, resStation, addBuffer, mulBuffer, loadBuffer, storeBuffer, cycle, instructionQueue,latencies){
     let pos = instructionQueue.instruction.indexOf(instruction)
-    instructionQueue.execute[pos] = cycle
     let stationNumber = parseInt(resStation.substring(1)) - 1
 
     if(resStation.include("A")){
+        instructionQueue.execute[pos] = cycle+".."+(cycle+latencies.addLatency-1)
         addBuffer[stationNumber].cyclesRemaining = latencies.addLatency
     }
     else if(resStation.include("M")){
+        instructionQueue.execute[pos] = cycle+".."+(cycle+latencies.mulLatency-1)
         mulBuffer[stationNumber].cyclesRemaining = latencies.mulLatency
     }
     else if(resStation.include("L")){
+        instructionQueue.execute[pos] = cycle+".."+(cycle+latencies.loadLatency-1)
         loadBuffer[stationNumber].cyclesRemaining = latencies.loadLatency
     }
     else if(resStation.include("S")){
+        instructionQueue.execute[pos] = cycle+".."+(cycle+latencies.storeLatency-1)
         storeBuffer[stationNumber].cyclesRemaining = latencies.storeLatency
     }
 }
